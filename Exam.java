17d98096fc4b5b39eb7fab60a166820ec0ce028f
Shridhar <shridharmane111999@gmail.com>
2023-02-02 14:32:55 +0530

    InsersionSort

--- Exam.java
+++ Exam.java
@@ -1,18 +1,34 @@
-class BubbleSort {
+import java.util.*;
+
+class Exam {
 	void bubbleSort(int arr[])
 	{
 		int n = arr.length;
 		for (int i = 0; i < n - 1; i++)
 			for (int j = 0; j < n - i - 1; j++)
 				if (arr[j] > arr[j + 1]) {
-					
+			
 					int temp = arr[j];
 					arr[j] = arr[j + 1];
 					arr[j + 1] = temp;
 				}
 	}
-
 	
+	void insertionSort(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+ 
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
 	void printArray(int arr[])
 	{
 		int n = arr.length;
@@ -24,9 +40,24 @@ class BubbleSort {
 
 	public static void main(String args[])
 	{
-		BubbleSort ob = new BubbleSort();
+		Exam ob = new Exam();
 		int arr[] = { 5, 1, 4, 2, 8 };
-		ob.bubbleSort(arr);
+		System.out.println("1. Bubble Sort\n2. Insertion Sort");
+		System.out.println("Enter your choice:");
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		switch(num){
+			case 1:
+				ob.bubbleSort(arr);
+				break;
+			case 2:
+				ob.insertionSort(arr);
+				break;
+			default:
+				System.out.println("Enter correct choice");
+				break;
+		}
+		
 		System.out.println("Sorted array");
 		ob.printArray(arr);
 	}
